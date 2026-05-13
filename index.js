62607f49761bd6f08d7a4a2ce35aeeeb3de223aa
Karolina <karolina.steinborn@gmail.com>
2026-05-13 16:55:10 +0000

    Add basic user API endpoints

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+let users = [];
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.status(201).json(user);
 });
 
 app.listen(port, () => {
