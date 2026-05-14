35b149ac69a31dae11f6a579c1aca1c9280d5d4e
Karolina <karolina.steinborn@gmail.com>
2026-05-14 09:16:35 +0000

    Add several errors for testing review MCP server: unused variable, division by zero, syntax error

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let unusedVariable = 'this is not used';
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/error', (req, res) => {
+  // Logical error: dividing by zero
+  let result = 10 / 0;
+  res.send('This will cause an error: ' + result);
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
-});
+  // Syntax error: missing closing bracket
+  console.log('This is fine');
