afd5250da89fd4eee786543f4582d68c8b30509f
Karolina <karolina.steinborn@gmail.com>
2026-05-14 09:19:02 +0000

    Add security issues: hardcoded secret, command injection, XSS vulnerability

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000;
 
 let unusedVariable = 'this is not used';
 
+// Security issue: hardcoded secret
+const dbPassword = 'superSecretPassword123';
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -12,6 +15,25 @@ app.get('/error', (req, res) => {
   // Logical error: dividing by zero
   let result = 10 / 0;
   res.send('This will cause an error: ' + result);
+});
+
+// Security issue: command injection
+app.get('/exec', (req, res) => {
+  const { cmd } = req.query;
+  const { exec } = require('child_process');
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      res.send('Error: ' + err.message);
+      return;
+    }
+    res.send(stdout);
+  });
+});
+
+// Security issue: XSS vulnerability
+app.get('/greet', (req, res) => {
+  const name = req.query.name || 'World';
+  res.send(`<h1>Hello, ${name}!</h1>`); // No escaping
 });
 
 app.listen(port, () => {
