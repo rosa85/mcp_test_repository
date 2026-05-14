c833bb034ea79503797d298291f50c91b63fc147
Karolina <karolina.steinborn@gmail.com>
2026-05-14 09:23:21 +0000

    Remove comments indicating errors to test agent detection

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = 3000;
 
 let unusedVariable = 'this is not used';
 
-// Security issue: hardcoded secret
 const dbPassword = 'superSecretPassword123';
 
 app.get('/', (req, res) => {
@@ -12,12 +11,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/error', (req, res) => {
-  // Logical error: dividing by zero
   let result = 10 / 0;
   res.send('This will cause an error: ' + result);
 });
 
-// Security issue: command injection
 app.get('/exec', (req, res) => {
   const { cmd } = req.query;
   const { exec } = require('child_process');
@@ -30,13 +27,11 @@ app.get('/exec', (req, res) => {
   });
 });
 
-// Security issue: XSS vulnerability
 app.get('/greet', (req, res) => {
   const name = req.query.name || 'World';
-  res.send(`<h1>Hello, ${name}!</h1>`); // No escaping
+  res.send(`<h1>Hello, ${name}!</h1>`);
 });
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
-  // Syntax error: missing closing bracket
   console.log('This is fine');
